366fa195f6ae9d799af8bbad737f878aedcdcaa6
theconartist <jacobcons@gmail.com>
2015-07-27 16:02:20 +0100

    fffffffffffffffffffffffffuu

--- assets/scripts/client.js
+++ assets/scripts/client.js
@@ -3,6 +3,10 @@ var io = require("socket.io-client");
 module.exports = function() {
     var socket = io(location.origin);
 
+    $(".artist-submit").click(function() {
+        socket.emit("artist", $("artist-name").val());
+    });
+
     socket.emit("artist", "whitechapel");
     socket.emit("tag", "jazzy");
 
