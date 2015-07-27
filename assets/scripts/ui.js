1ab35489a5597f5c19276f330ee102367867060e
theconartist <jacobcons@gmail.com>
2015-07-27 17:13:07 +0100

    fgadfg[opjaer]gpo'kl

--- assets/scripts/ui.js
+++ assets/scripts/ui.js
@@ -2,10 +2,10 @@ module.exports = function() {
     $('.modal-trigger').click(function () {
           $('#tool-box').openModal();
     });
-	$('#sortable').sortable({
+	$('.sortable').sortable({
 		axis: "y"
 	});
-    
+
     $(document).ready(function() {
         $('.parallax').parallax();
     });
