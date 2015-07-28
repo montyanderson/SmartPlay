4b31dcb22a7c8a3b117d5b67b3af669e980e7747
Monty Anderson <monty.linux@gmail.com>
2015-07-28 11:17:53 +0100

    added viewport tag

--- sockets.js
+++ sockets.js
@@ -3,18 +3,25 @@ var lastfm = require("./lastfm.js");
 module.exports = function(io) {
     io.on("connection", function(socket) {
         socket.on("artist", function(artist) {
-            console.log("search: " + artist);
-            lastfm({
-                method: "artist.search",
-                artist: artist
-            }, function(data) {
-                if(data.results.artistmatches.artist) {
+            if(typeof artist === "string") {
+                console.log("search: " + artist);
+                lastfm({
+                    method: "artist.search",
+                    artist: artist
+                }, function(data) {
                     console.log(data);
-                    socket.emit("artist", data.results.artistmatches.artist[0]);
-                } else {
-                    socket.emit("err", "Sorry, no matches for that artist were found.");
-                }
-            });
+
+                    if(data.results.artistmatches !== "\n") {
+                        socket.emit("artist", data.results.artistmatches.artist[0]);
+                    } else {
+                        socket.emit("alert", "Sorry, no matches for that artist were found.");
+                    }
+                });
+            }
+        });
+
+        socket.on("tag", function(tag) {
+
         });
     });
 };
