c9b34f719721b3cb55b9279bec75d82b3a788a6a
theconartist <jacobcons@gmail.com>
2015-07-28 12:29:35 +0100

    animations

--- assets/scripts/ui.js
+++ assets/scripts/ui.js
@@ -5,6 +5,17 @@ module.exports = function() {
     $('.modal-trigger').click(function () {
           $($(this).attr("href")).openModal();
     });
+    $('.btn-expand').hover(function () {
+        $(this).animate({
+            $(this).attr('data-direction'):'50px',
+            'border-radius':'20px'
+        }, 1);
+    }, function () {
+        $(this).animate({
+            $(this).attr('data-direction'):'0px',
+            'border-radius':'100px'
+        }, 1);
+    });
     $(document).ready(function() {
         $('.parallax').parallax();
     });
