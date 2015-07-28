c4a477eb1c3756353030fee69bb4ab121bf5950e
theconartist <jacobcons@gmail.com>
2015-07-28 20:32:08 +0100

    a humble start

--- generator.js
+++ generator.js
@@ -1,5 +1,7 @@
 var lastfm = require("./lastfm.js");
 
 module.exports = function(data) {
-    console.log(data);
+    var len = data.length + 1,
+        totalTracks = 15
+    console.log(len);
 };
