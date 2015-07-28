1060292787f1f5fe5f13ffb964e651d50082edea
Monty Anderson <monty.linux@gmail.com>
2015-07-28 17:00:18 +0100

    few changes

--- generator.js
+++ generator.js
@@ -2,6 +2,13 @@ var lastfm = require("./lastfm.js");
 
 module.exports = function(data) {
     data.forEach(function(obj) {
-        console.log(obj);
+        if(obj.type === "artist") {
+            lastfm({
+                method: "artist.gettopalbums",
+                artist: obj.name
+            }, function(albums) {
+                console.log(albums)
+            });
+        }
     });
 };
