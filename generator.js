9fb4a1e6ad7d18638b6dea9b0ad02c401313f9d4
theconartist <jacobcons@gmail.com>
2015-07-28 22:29:24 +0100

    trackNum_script_update

--- generator.js
+++ generator.js
@@ -11,34 +11,44 @@ module.exports = function(data) {
 
     function convert () {
         var naturalSum = (len / 2) * (len + 1),
-            totalTracks = 15;
+            totalTracks = 50;
 
         if (totalTracks >= naturalSum) {
-          for (i = 0; i < len; i++) {
-              descendArr[i] = Math.round(descendArr[i] / naturalSum * totalTracks);
-          }
-          return descendArr;
-        } else if (totalTracks < len) {
-          return "Reduce the number of artists!";
-        } else {
-          var loopCount = 0,
-              difference = naturalSum - totalTracks;
-          len--;
-          while (loopCount < difference) {
+            var sum = 0;
             for (i = 0; i < len; i++) {
-              if (descendArr[i] > 1 && loopCount < difference) {
-                descendArr[i]--;
-                loopCount++;
-            } else if (descendArr[i] == 1) {
-                len--;
-              } else {
-                break;
-              }
+                sum += Math.round(descendArr[i] / naturalSum * totalTracks);
+                descendArr[i] = Math.round(descendArr[i] / naturalSum * totalTracks);
             }
-          }
-          return descendArr;
+            if (sum == totalTracks + 1) {
+                descendArr[len-1]--;
+            }
+            return descendArr;
+        } else if (totalTracks < len) {
+            for (i = 0; i < len; i++) {
+                descendArr[i] = 1;
+            }
+            return descendArr;
+        } else {
+              var loopCount = 0,
+                  difference = naturalSum - totalTracks;
+                  len--;
+              while (loopCount < difference) {
+                for (i = 0; i < len; i++) {
+                  if (descendArr[i] > 1 && loopCount < difference) {
+                    descendArr[i]--;
+                    loopCount++;
+                } else if (descendArr[i] == 1) {
+                    len--;
+                  } else {
+                    break;
+                  }
+                }
+              }
+              len = data.length;
+              return descendArr;
         }
     }
+
     var convertContainer = convert();
     for (i = 0; i < len; i++) {
         data[i].trackNum = convertContainer[i];
